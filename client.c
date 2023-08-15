812d4beba083e5b2f038d5b04d218a72bb210792
Mr. Norkum <kelestayyib@gmail.com>
2023-08-15 05:53:25 +0300

    Successful

--- client.c
+++ client.c
@@ -8,9 +8,9 @@ static inline int	my_atoi(const char *str, int sign, int res, int mod)
 	if (*str == 43 && mod == 0)
 		return (my_atoi(str + 1, 1, 0, 1));
 	if (*str == 45 && mod == 0)
-	    return (my_atoi(str + 1, -1, 0, 1));
+	    	return (my_atoi(str + 1, -1, 0, 1));
 	if ('0' <= *str && *str <= '9')
-		return (my_atoi(str + 1, sign, res = res * 10 + *str - 48, 1));
+		return (my_atoi(str + 1, sign, res * 10 + *str - 48, 1));
 	return (res * sign);
 }
 
