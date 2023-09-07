1d81c7888331c3dbc9cc2a8aac1876288bee2293
Mr. Norkum <kelestayyib@gmail.com>
2023-09-07 06:45:15 +0300

    Successful

--- client.c
+++ client.c
@@ -7,7 +7,7 @@ static inline int	my_atoi(const char *str, int sign, int res, int mod)
 	if (*str == 43 && mod == 0)
 		return (my_atoi(str + 1, 1, 0, 1));
 	if (*str == 45 && mod == 0)
-	    return (my_atoi(str + 1, -1, 0, 1));
+		return (my_atoi(str + 1, -1, 0, 1));
 	if ('0' <= *str && *str <= '9')
 		return (my_atoi(str + 1, sign, res * 10 + *str - 48, 1));
 	return (res * sign);
@@ -24,7 +24,7 @@ static inline void	signal_sender(int pid, char c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(150);
+		usleep(200);
 	}
 }
 
