b373e38f581c8068f05b6b77dd5b06de9dbf3a4b
Mr. Norkum <kelestayyib@gmail.com>
2023-12-11 01:39:16 +0300

    Successful

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <signal.h>
+#include <stdbool.h>
 
 static void	received_signal(int sig)
 {
@@ -7,59 +8,53 @@ static void	received_signal(int sig)
 		write(1, "Signal Received\n", 16);
 }
 
-static void	err_client(void)
+static char	err_client(void)
 {
 	write(1, "$---------------------------------$\n", 36);
 	write(1, "~> ./client <PID> <STRING to send>\n", 35);
 	write(1, "$---------------------------------$\n", 36);
-
+	return (1);
 }
 
-static int	my_atoi(const char *s, char sign, int res, char mod)
+static int	my_atoi(const char *s, bool sign, int res, bool mod)
 {
 	if (((9 <= *s && *s <= 13) || *s == 32) && !mod)
-		return (my_atoi(s + 1, 0, 0, 0));
+		return (my_atoi(s + 1, false, 0, false));
 	if (*s == 43 && !mod)
-		return (my_atoi(s + 1, 0, 0, 1));
+		return (my_atoi(s + 1, false, 0, true));
 	if (*s == 45 && !mod)
-		return (my_atoi(s + 1, 1, 0, 1));
+		return (my_atoi(s + 1, true, 0, true));
 	if ('0' <= *s && *s <= '9')
-		return (my_atoi(s + 1, sign, (res * 10) + (*s & 15), 1));
+		return (my_atoi(s + 1, sign, (res * 10) + (*s & 15), true));
 	if (sign)
-		res = 1 + ~res;
+		return (1 + ~res);
 	return (res);
 }
 
 static void	signal_sender(int pid, char c)
 {
-	int	i;
+	register int	i;
 
 	i = -1;
 	while (++i < 8)
 	{
-		if (c << i & 128)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
+		kill(pid, SIGUSR1 + !(c << i & 128));
 		usleep(200);
 	}
 }
 
 int	main(int ac, char **av)
 {
-	int	pid;
+	register int	pid;
 
 	if (ac == 3)
 	{
-		pid = my_atoi(av[1], 1, 0, 0);
-		if (!pid)
-			return (err_client(), 1);
+		pid = my_atoi(av[1], false, 0, false);
 		signal(SIGUSR2, received_signal);
 		while (*av[2])
 			signal_sender(pid, *av[2]++);
 		signal_sender(pid, '\0');
+		return (0);
 	}
-	else
-		return (err_client(), 1);
-	return (0);
+	return (err_client());
 }
